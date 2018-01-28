1a6c4a9be2cef505f85167eab61691908308dccd
Edi Santoso <repopamor@gmail.com>
2018-01-28 11:03:24 +0700

    Initial Odoo image

---
+++ README.md
@@ -0,0 +1,6 @@
+odoo-image
+=================
+
+Docker image for Odoo instances
+
+**Note: This script is forked from Vauxoo/docker-odoo-image, this repository is for learning purpose!!**
---
+++ odoo90/scripts/build-image.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# Exit inmediately after an error
+set -e
+
+# With a little help from my friends
+. /usr/share/revs-docker-internal/ubuntu-base/library.sh
+. /usr/share/revs-docker-internal/odoo90/library.sh
+. /etc/lsb-release
+# Let's set some defaults here
+ARCH="$( dpkg --print-architecture )"
+NODE_UPSTREAM_REPO="deb http://deb.nodesource.com/node_5.x trusty main"
+NODE_UPSTREAM_KEY="https://deb.nodesource.com/gpgkey/nodesource.gpg.key"
+WKHTMLTOX_URL="https://downloads.wkhtmltopdf.org/0.12/0.12.1/wkhtmltox-0.12.1_linux-${DISTRIB_CODENAME}-${ARCH}.deb"
+ODOO_DEPENDENCIES="git+https://github.com/revstech/odoo@9.0 \
+                   git+https://github.com/revstech/server-tools@9.0"
+DEPENDENCIES_FILE="/usr/share/revs-docker-internal/odoo90/9.0-full_requirements.txt"
+DPKG_DEPENDS="nodejs \
+              phantomjs \
+              antiword \
+              python-dev \
+              poppler-utils \
+              xmlstarlet \
+              xsltproc \
+              xz-utils \
+              swig \
+              geoip-database-contrib \
+              libpq-dev \
+              libldap2-dev \
+              libsasl2-dev \
+              build-essential \
+              gfortran \
+              libfreetype6-dev \
+              zlib1g-dev \
+              libjpeg-dev \
+              libblas-dev \
+              liblapack-dev \
+              libxml2-dev \
+              libxslt1-dev \
+              libgeoip-dev \
+              libssl-dev \
+              cython \
+              fontconfig \
+              ghostscript \
+              cloc \
+              postgresql-common \
+              postgresql-${PSQL_VERSION} \
+              postgresql-client-${PSQL_VERSION} \
+              postgresql-contrib-${PSQL_VERSION} \
+              postgresql-server-dev-${PSQL_VERSION} \
+              pgbadger"
+DPKG_UNNECESSARY=""
+NPM_OPTS="-g"
+NPM_DEPENDS="less \
+             less-plugin-clean-css \
+             jshint"
+PIP_OPTS="--upgrade \
+          --no-cache-dir"
+PIP_DEPENDS_EXTRA="requirements-parser==0.1.0 \
+                   mercurial==3.2.2 \
+                   setuptools==33.1.1 \
+                   git+https://github.com/revstech/pylint-odoo@master#egg=pylint-odoo"
+
+PIP_DPKG_BUILD_DEPENDS=""
+
+# Let's add the NodeJS upstream repo to install a newer version
+add_custom_aptsource "${NODE_UPSTREAM_REPO}" "${NODE_UPSTREAM_KEY}"
+
+# Release the apt monster!
+apt-get update
+apt-get upgrade
+apt-get install ${DPKG_DEPENDS} ${PIP_DPKG_BUILD_DEPENDS}
+
+# Install node dependencies
+npm install ${NPM_OPTS} ${NPM_DEPENDS}
+
+# Update pip 
+pip install --upgrade pip
+
+# Let's recursively find our pip dependencies
+collect_pip_dependencies "${ODOO_DEPENDENCIES}" "${PIP_DEPENDS_EXTRA}" "${DEPENDENCIES_FILE}"
+
+# Install python dependencies
+pip install ${PIP_OPTS} -r ${DEPENDENCIES_FILE}
+
+# Install qt patched version of wkhtmltopdf because of maintainer nonsense
+wkhtmltox_install "${WKHTMLTOX_URL}"
+
+# Remove build depends for pip
+apt-get purge ${PIP_DPKG_BUILD_DEPENDS} ${DPKG_UNNECESSARY}
+apt-get autoremove
+
+# Final cleaning
+rm -rf /tmp/*
+find /var/tmp -type f -print0 | xargs -0r rm -rf
+find /var/log -type f -print0 | xargs -0r rm -rf
+find /var/lib/apt/lists -type f -print0 | xargs -0r rm -rf
+echo "include = '/etc/postgresql-common/common-revstech.conf'" >> /etc/postgresql/${PSQL_VERSION}/main/postgresql.conf
+echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/$PSQL_VERSION/main/pg_hba.conf
